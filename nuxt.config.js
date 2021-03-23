33feec61435644fac37d484fdce78aa7c378f08f
Pooh <sophon@punchup.world>
2021-03-23 15:15:23 +0700

    Change file to docs

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
   generate: {
-    dir: 'doc',
+    dir: 'docs',
   },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
